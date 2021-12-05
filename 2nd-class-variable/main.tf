6ba52fe3744de019b685ac829859d943ed70b682
er.kapilkumar0001@gmail.com <er.kapilkumar0001@gmail.com>
2021-12-05 14:10:32 +0530

    adding local and output varibales

--- 2nd-class-variable/main.tf
+++ 2nd-class-variable/main.tf
@@ -17,14 +17,28 @@ variable "instance_type" {
   type = string
 }
 
+#Define local variable
+
+locals {
+  project_name = "DIS"
+}
+
 resource "aws_instance" "webserver1" {
   ami = "ami-0fed77069cd5a6d6c"
 # instance_type = "t2.nano"
   instance_type = var.instance_type
   key_name = "singapore"
   
-
+#USing local varibales 
   tags = {
-    Name = "myserver1"
+    Name = "myserver1-${local.project_name}"
   }
+}
+#output variables
+output "instance-public-ip"{
+  value = aws_instance.webserver1.public_ip
+}
+
+output "instance-DNS-ip"{
+  value = aws_instance.webserver1.public_dns
 }
