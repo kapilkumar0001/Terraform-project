110caf1e105454e11fcbc1a67b8e69e101f68444
er.kapilkumar0001@gmail.com <er.kapilkumar0001@gmail.com>
2021-12-07 15:04:01 +0530

    lifecycle set to true so not destory the infra

---
+++ 13th-class-resource-meta-argument-lifecycle/main.tf
@@ -0,0 +1,36 @@
+ terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.68.0"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "ap-southeast-1"
+  # Configuration options
+} 
+
+
+resource "aws_instance" "webserver1" {
+  ami = "ami-0fed77069cd5a6d6c"
+  instance_type = "t2.nano"
+  key_name = "singapore"
+#USing local varibales 
+  tags = {
+    Name = "myserver1"
+  }
+  lifecycle {
+        prevent_destroy = true
+  } 
+} 
+#output variables
+output "instance-public-ip1"{
+  value = aws_instance.webserver1.public_ip
+}
+/*output "instance-DNS-ip"{
+  value = aws_instance.webserver1.public_dns
+}
+*/
